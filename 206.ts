aa413351a405342a0aa8b8f7c995832c7cc81773
MOONYAN <c8763sasuke@ntut.org.tw>
2020-12-15 10:38:16 +0800

    206 solved

---
+++ 206.ts
@@ -0,0 +1,19 @@
+import { ListNode } from "./shared/list-node";
+
+function reverseList(head: ListNode | null): ListNode | null {
+
+    if(head === null){
+        return null;
+    }
+    let list = [];
+
+    for (let node = head; node != null; node = node ? node.next : null) {
+        list.push(node);
+    }
+    for (let i = 1; i < list.length; i++) {
+        list[i].next = list[i-1];
+    }
+    list[0].next=null;
+
+    return list[list.length - 1];
+}
