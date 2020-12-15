43545ce05041a2eee27a9ab3dad1d863786467e1
MOONYAN <c8763sasuke@ntut.org.tw>
2020-12-15 10:57:58 +0800

    apply with shared struct

--- 617.ts
+++ 617.ts
@@ -1,15 +1,6 @@
 // https://leetcode.com/problems/merge-two-binary-trees/
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.left = (left === undefined ? null : left)
-        this.right = (right === undefined ? null : right)
-    }
-}
 
+import { TreeNode } from "./shared/tree-node";
 
 function mergeTrees(t1: TreeNode | null, t2: TreeNode | null): TreeNode | null {
     if (t1 === null && t2 === null) {
@@ -19,4 +10,4 @@ function mergeTrees(t1: TreeNode | null, t2: TreeNode | null): TreeNode | null {
     root.left = mergeTrees(t1 ? t1.left : null, t2 ? t2.left : null);
     root.right = mergeTrees(t1 ? t1.right : null, t2 ? t2.right : null);
     return root;
-};
+}
