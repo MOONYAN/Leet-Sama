492b63a3066fcb2850ec2bdd972d66ff47f09223
MOONYAN <c8763sasuke@ntut.org.tw>
2021-03-16 17:52:40 +0800

    322 updated

--- 322.ts
+++ 322.ts
@@ -1,5 +1,19 @@
 // https://leetcode.com/problems/coin-change/
 
+function coinChange(coins: number[], amount: number): number {
+
+    let dp: number[] = Array(amount + 1).fill(Number.MAX_VALUE);
+    dp[0] = 0;
+
+    coins.forEach(c => {
+        for (let i = c; i <= amount; i++) {
+            dp[i] = Math.min(dp[i - c] + 1, dp[i]);
+        }
+    })
+
+    return (dp[amount] != Number.MAX_VALUE) ? dp[amount] : -1;
+}
+
 // function coinChange(coins: number[], amount: number): number {
 //     if (amount === 0)
 //         return 0;
@@ -13,19 +27,3 @@
 //     }
 //     return (dp[amount] === undefined) ? -1 : dp[amount];
 // };
-
-function coinChange(coins: number[], amount: number): number {
-
-    let dp: number[] = Array(amount + 1).fill(Number.MAX_VALUE);
-    dp[0] = 0;
-
-    coins.forEach(c => {
-        for (let i = c; i <= amount; i++) {
-            if (dp[i - c] != Number.MAX_VALUE) {
-                dp[i] = Math.min(dp[i - c] + 1, dp[i]);
-            }
-        }
-    })
-
-    return (dp[amount] != Number.MAX_VALUE) ? dp[amount] : -1;
-}
