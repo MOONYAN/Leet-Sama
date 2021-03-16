0db274dc0c864ac0c10d1ae881d2dcab3139dfdc
MOONYAN <c8763sasuke@ntut.org.tw>
2021-03-16 17:44:32 +0800

    322 updated

--- 322.ts
+++ 322.ts
@@ -1,14 +1,31 @@
 // https://leetcode.com/problems/coin-change/
+
+// function coinChange(coins: number[], amount: number): number {
+//     if (amount === 0)
+//         return 0;
+//     coins.sort((a, b) => b - a);
+//     let dp: number[] = [0];
+//     for (let i = 1; i <= amount; i++) {
+//         let vals: number[] = coins.filter(c=>i>=c && dp[i-c]!==undefined).map(c=>dp[i-c]+1);
+//         if(vals.length>0){
+//             dp[i] = Math.min(...vals);
+//         }        
+//     }
+//     return (dp[amount] === undefined) ? -1 : dp[amount];
+// };
+
 function coinChange(coins: number[], amount: number): number {
-    if (amount === 0)
-        return 0;
-    coins.sort((a, b) => b - a);
-    let dp: number[] = [0];
-    for (let i = 1; i <= amount; i++) {
-        let vals: number[] = coins.filter(c=>i>=c && dp[i-c]!==undefined).map(c=>dp[i-c]+1);
-        if(vals.length>0){
-            dp[i] = Math.min(...vals);
-        }        
-    }
-    return (dp[amount] === undefined) ? -1 : dp[amount];
-};
+
+    let dp: number[] = Array(amount + 1).fill(Number.MAX_VALUE);
+    dp[0] = 0;
+
+    coins.forEach(c => {
+        for (let i = c; i <= amount; i++) {
+            if (dp[i - c] != Number.MAX_VALUE) {
+                dp[i] = Math.min(dp[i - c] + 1, dp[i]);
+            }
+        }
+    })
+
+    return (dp[amount] != Number.MAX_VALUE) ? dp[amount] : -1;
+}
