6766050c18946d81fab7be9f8ac6d725900af3bc
MOONYAN <c8763sasuke@ntut.org.tw>
2021-03-17 14:30:28 +0800

    198 solved

---
+++ 198.ts
@@ -0,0 +1,21 @@
+function rob(nums: number[]): number {
+
+    const N: number = nums.length;
+
+    if (N <= 2) {
+        return Math.max(...nums);
+    }
+
+    // maximum value at house i
+    let dp: number[] = Array(N).fill(0);
+
+    dp[0] = nums[0];
+
+    dp[1] = Math.max(dp[0], nums[1]);
+
+    for (let i = 2; i < N; i++) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    }
+
+    return dp[N - 1];
+}
