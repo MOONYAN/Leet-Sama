47dca236cb1109bfa5199ce17f0bbdae7aa5424b
MOONYAN <c8763sasuke@ntut.org.tw>
2021-03-19 14:57:04 +0800

    344 solved

---
+++ 344.ts
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/reverse-string/
+function reverseString(s: string[]): void {
+
+    for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+
+        [s[i], s[j]] = [s[j], s[i]]
+    }
+}
