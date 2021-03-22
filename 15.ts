58d22fd46689a58050782dd98246da618aa672a5
MOONYAN <c8763sasuke@ntut.org.tw>
2021-03-22 18:15:11 +0800

    15 updated

--- 15.ts
+++ 15.ts
@@ -1,20 +1,48 @@
 // https://leetcode.com/problems/3sum/
-function threeSum(nums: number[]): number[][] {
-    const len = nums.length;
+
+function help(nums: number[], target: number, start: number): number[][] {
+
+    const N = nums.length;
+
     let output: number[][] = [];
-    nums.sort((a, b) => a - b);
-    for (let i = 0, hashi = new Set<number>(); i < len - 2 && nums[i] <= 0; i++) {
-        if (hashi.has(nums[i]))
-            continue;
-        for (let j = i + 1, hashj = new Set<number>(), hashk = new Set<number>(); j < len; j++) {
-            const rest = -(nums[i] + nums[j]);
-            if (hashj.has(rest) && !hashk.has(rest)) {
-                output.push([nums[i], rest, nums[j]])
-                hashk.add(rest);
-            }
-            hashj.add(nums[j]);
+
+    let idx_r = start;
+    let idx_l = N - 1;
+
+    while (idx_r < idx_l) {
+        const sum = nums[idx_r] + nums[idx_l];
+        if (target + sum === 0) {
+            output.push([target, nums[idx_r], nums[idx_l]]);
+            idx_l--;
+            idx_r++;
+            while (nums[idx_l] === nums[idx_l + 1]) { idx_l--; }
+            while (nums[idx_r] === nums[idx_r - 1]) { idx_r++; }
+        } else {
+            (sum > -target) ? idx_l-- : idx_r++;
         }
+    }
+
+    return output;
+}
+
+function threeSum(nums: number[]): number[][] {
+
+    const N = nums.length;
+
+    let output: number[][] = [];
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0, hashi = new Set<number>(); i < N - 2 && nums[i] <= 0; i++) {
+
+        const target = nums[i];
+
+        if (hashi.has(target))
+            continue;
+
+        output.push(...help(nums, target, i + 1));
+
         hashi.add(nums[i]);
     }
     return output;
-};
+}
