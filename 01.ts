33cf33cfd320041ef69d154b38511a7c73c1269b
MOONYAN <c8763sasuke@ntut.org.tw>
2021-03-22 15:23:18 +0800

    01 updated

--- 01.ts
+++ 01.ts
@@ -1,12 +1,19 @@
 // https://leetcode.com/problems/two-sum/
 function twoSum(nums: number[], target: number): number[] {
-    const len = nums.length;
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < len; j++) {
-            if (nums[i] === target - nums[j]) {
-                return [i, j];
-            }
+
+    const N = nums.length;
+
+    let map: { [key: number]: number } = {};
+
+    for (let i = 0; i < N; i++) {
+
+        const idx = map[target - nums[i]];
+
+        if (idx !== undefined) {
+            return [idx, i];
         }
+
+        map[nums[i]] = i;
     }
     return [];
-};
+}
